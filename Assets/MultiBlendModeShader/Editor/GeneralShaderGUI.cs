e2d2ec420e495454f2d76e81f6c3db55965e2f4e
gam0022 <gam0022@gmail.com>
2019-12-06 10:28:38 +0900

    first commit

---
+++ Assets/MultiBlendModeShader/Editor/GeneralShaderGUI.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// ブレンドモード切替可能なシェーダーのインスペクタ拡張
+/// </summary>
+public class GeneralShaderGUI : ShaderGUI
+{
+    /// <summary>
+    /// ブレンドモード
+    /// </summary>
+    public enum BlendMode
+    {
+        Opaque,
+        Cutout,
+        Transparent,
+        Additive,
+        Multiply,
+    }
+
+    /// <summary>
+    /// BlendModeのインスペクタを描画します
+    /// </summary>
+    /// <param name="materialEditor">MaterialEditor</param>
+    /// <param name="properties">Properties</param>
+    void DrawBlendMode(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        var blendMode = FindProperty("_BlendMode", properties);
+        var cutoff = FindProperty("_Cutoff", properties);
+        var mode = (BlendMode) blendMode.floatValue;
+
+        using (var scope = new EditorGUI.ChangeCheckScope())
+        {
+            mode = (BlendMode) EditorGUILayout.Popup("Blend Mode", (int) mode, Enum.GetNames(typeof(BlendMode)));
+
+            if (scope.changed)
+            {
+                blendMode.floatValue = (float) mode;
+                foreach (UnityEngine.Object obj in blendMode.targets)
+                {
+                    ApplyBlendMode(obj as Material, mode);
+                }
+            }
+        }
+
+        if (mode == BlendMode.Cutout)
+        {
+            materialEditor.ShaderProperty(cutoff, cutoff.displayName);
+        }
+    }
+
+    /// <summary>
+    /// インスペクタを拡張します
+    /// </summary>
+    /// <param name="materialEditor">MaterialEditor</param>
+    /// <param name="properties">Properties</param>
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        DrawBlendMode(materialEditor, properties);
+
+        var mainTex = FindProperty("_MainTex", properties);
+        materialEditor.ShaderProperty(mainTex, mainTex.displayName);
+
+        var tintColor = FindProperty("_TintColor", properties);
+        materialEditor.ShaderProperty(tintColor, tintColor.displayName);
+
+        materialEditor.RenderQueueField();
+    }
+
+    /// <summary>
+    /// マテリアルのブレンドモードを変更します
+    /// </summary>
+    /// <param name="material">マテリアル</param>
+    /// <param name="blendMode">ブレンドモード</param>
+    static void ApplyBlendMode(Material material, BlendMode blendMode)
+    {
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                material.SetOverrideTag("RenderType", "");
+                material.SetInt("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int) UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.renderQueue = -1;
+                break;
+
+            case BlendMode.Cutout:
+                material.SetOverrideTag("RenderType", "TransparentCutout");
+                material.SetInt("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int) UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.EnableKeyword("_ALPHATEST_ON");
+                material.renderQueue = (int) UnityEngine.Rendering.RenderQueue.AlphaTest;
+                break;
+
+            case BlendMode.Transparent:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetInt("_DstBlend", (int) UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.renderQueue = (int) UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+
+            case BlendMode.Additive:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetInt("_DstBlend", (int) UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.renderQueue = (int) UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+
+            case BlendMode.Multiply:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.DstColor);
+                material.SetInt("_DstBlend", (int) UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.renderQueue = (int) UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException("blendMode", blendMode, null);
+        }
+    }
+
+    /// <summary>
+    /// シェーダー切り替え時の処理をします
+    /// </summary>
+    /// <param name="material">マテリアル</param>
+    /// <param name="oldShader">切り替え前のシェーダー</param>
+    /// <param name="newShader">切り替え後のシェーダー</param>
+    public override void AssignNewShaderToMaterial(Material material, Shader oldShader, Shader newShader)
+    {
+        base.AssignNewShaderToMaterial(material, oldShader, newShader);
+
+        // MaterialのShader切り替え時にBlend指定が変更されてしまうので再設定
+        ApplyBlendMode(material, (BlendMode) material.GetFloat("_BlendMode"));
+    }
+}
---
+++ Assets/MultiBlendModeShader/General.shader
@@ -0,0 +1,87 @@
+﻿Shader "Unlit/General"
+{
+    Properties
+    {
+        // Blending state
+        [HideInInspector] _BlendMode ("__mode", Float) = 0.0
+        [HideInInspector] _SrcBlend ("__src", Float) = 1.0
+        [HideInInspector] _DstBlend ("__dst", Float) = 0.0
+        [HideInInspector] _ZWrite ("__zw", Float) = 1.0
+
+        // Cutoutの閾値
+        _Cutoff("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
+
+        // メインテクスチャ
+        _MainTex ("Texture", 2D) = "white" {}
+
+        // 乗算カラー
+        [HDR] _TintColor("Tint Color",  Color) = (1.0, 1.0, 1.0, 1.0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            Blend[_SrcBlend][_DstBlend]
+            ZWrite [_ZWrite]
+            Cull Back
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #pragma multi_compile __ _ALPHATEST_ON
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _Cutoff;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                #ifdef _ALPHATEST_ON
+                {
+                    clip(col.a - _Cutoff);
+                }
+                #endif
+
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+    Fallback "Unlit/Texture"
+    CustomEditor "GeneralShaderGUI"
+}
